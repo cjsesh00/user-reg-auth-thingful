db0032971e2d6ce63040f3a1708e27c16a7f7934
cjsesh00 <cjsesh2018@gmail.com>
2020-04-12 01:22:44 -0600

    User Reg complete server

---
+++ test/setup.js
@@ -0,0 +1,16 @@
+process.env.TZ = 'UTC'
+process.env.NODE_ENV = 'test'
+process.env.JWT_SECRET = 'test-jwt-secret'
+
+require('dotenv').config()
+
+process.env.TEST_DB_URL =
+    process.env.TEST_DB_URL || 'postgresql://thingful@localhost/thingful-test';
+
+const {
+    expect
+} = require('chai')
+const supertest = require('supertest')
+
+global.expect = expect
+global.supertest = supertest
